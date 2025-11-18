d827f1f11a3a30a3b834317c1eff3ddefeb46c1b
Oka <lehaloot@mail.ru>
2025-11-18 17:18:43 +0300

    Реализация функции useHttp

--- src/composables/useHttp.ts
+++ src/composables/useHttp.ts
@@ -8,13 +8,41 @@ function useHttp<T>() {
     const isSuccess = ref<boolean>(false)
     const error = ref<any | null>(null)
 
-    const execuite = async (url: string, options?: RequestInit) => {
+    const execute = async (url: string, options?: RequestInit) => {
+        isLoading.value = true
+        isError.value = false
+        isSuccess.value = false
+        error.value = null
+        data.value = null
+
+        try {
+            const response = await fetch(url, options)
+            status.value = response.status
+
+            if (!response.ok) {
+                isError.value = true
+                error.value = {
+                    message: `HTTP error! status: ${response.status}`,
+                    status: response.status
+                }
+                return
+            }
+            data.value = await response.json()
+            isSuccess.value = true
+        } catch (err) {
+            isError.value = true
+            error.value = err
+        } finally {
+            isLoading.value = false
+        }
+    }
     
     return {
         data,
         status,
         isLoading,
         isSuccess,
-        error
+        error,
+        execute
     }
 }
